a4aa461d59e0470444a666cbac6f958d7917e13f
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2024-05-13 11:00:57 +0900

    kakaoMap insert
    지도 생성 시도중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Select from '../components/Select'
-import styled from '@emotion/styled'
+import { Map } from 'react-kakao-maps-sdk'
+
+
 
 const Home = () => {
   return (
     <div>
-      <Select />
+      {/* <Select /> */}
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: "400px",
+          height: "350px",
+        }}
+        level={3} // 지도의 확대 레벨
+      />
     </div>
   )
 }
