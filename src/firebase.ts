985d0770fe0a289b35f987e1b0da47eb11e3aa88
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2024-06-03 11:01:33 +0900

    change maps align's, add firebase
    지도 배치 수정, firebase 도입위한 작업 시작

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClYBRk7SimrH4_R-hHxqCAvbOUUsy6u9o",
+  authDomain: "what-to-eat-7499d.firebaseapp.com",
+  projectId: "what-to-eat-7499d",
+  storageBucket: "what-to-eat-7499d.appspot.com",
+  messagingSenderId: "434349892646",
+  appId: "1:434349892646:web:92f7bcf458b953fdf1b2e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,46 @@
 import React from 'react'
 import Select from '../components/Select'
 import { Map } from 'react-kakao-maps-sdk'
+import styled from '@emotion/styled'
 
 
 
 const Home = () => {
   return (
-    <div>
-      {/* <Select /> */}
-      <Map // 지도를 표시할 Container
-        id="map"
-        center={{
-          // 지도의 중심좌표
-          lat: 33.450701,
-          lng: 126.570667,
-        }}
-        style={{
-          // 지도의 크기
-          width: "400px",
-          height: "350px",
-        }}
-        level={3} // 지도의 확대 레벨
-      />
-    </div>
+    <>
+      <Wrapper>
+        <Select />
+        <MapWrapper>
+          <Map // 지도를 표시할 Container
+            id="map"
+            center={{
+              // 지도의 중심좌표
+              lat: 33.450701,
+              lng: 126.570667,
+            }}
+            style={{
+              // 지도의 크기
+              width: "700px",
+              height: "600px",
+            }}
+            level={3} // 지도의 확대 레벨
+            
+          />
+        </MapWrapper>
+
+      </Wrapper>
+    </>
   )
 }
 
 export default Home
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10%;
+`
+
+const MapWrapper = styled.div`
+  margin-top: 100px;
+`;
